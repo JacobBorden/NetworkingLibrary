0e298770d81a98ba5d3bd12fbb4bccca6630d56a
Jacob Borden <amenra.beats@gmail.com>
2022-12-02 16:55:35 -0400

    Initial Commit

---
+++ src/client/client.cpp
@@ -0,0 +1,116 @@
+#include "client.h"
+
+Networking::Client::Client()
+{
+    try {
+        Client::InitClientSocket();
+    }
+
+    catch(int errorCode){
+        std::cout<<"Exception thrown. Error Code"<<errorCode;
+    }
+}
+
+Networking::Client::Client(char* _pHost, int _pPortNumber)
+{
+    try{
+        Client::InitClientSocket();
+        Client::CreateClientTCPSocket(_pHost, _pPortNumber);
+        Client::ConnectClientSocket();
+    }
+    catch(int errorCode){
+        std::cout<<"Exception thrown. Error Code "<<errorCode;
+    }
+}
+
+Networking::Client::~Client()
+{
+}
+
+bool Networking::Client::InitClientSocket()
+{
+    #ifdef _WIN32
+    int errorCode = WSAStartup(VERSIONREQUESTED, &this->wsaData);
+    if(errorCode)
+        throw errorCode;
+    #endif
+    return true;
+}
+
+bool Networking::Client::Client::CreateClientTCPSocket(char* _pHost, int _pPort)
+{
+    
+    ZeroMemory(&addressInfo, sizeof(addressInfo));
+    Client::SetFamily(AF_INET);
+    Client::SetSocketType(SOCK_STREAM);
+    Client::SetProtocol(IPPROTO_TCP); 
+
+    int errorCode = getaddrinfo((PCSTR)_pHost, (PCSTR)getservbyport(_pPort, NULL)->s_name,(const addrinfo*) &addressInfo,&hostAddressInfo );
+
+if(errorCode)
+{
+    #ifdef _WIN32
+    WSACleanup();
+    #endif
+    throw errorCode;
+}
+
+    connectionSocket = socket(hostAddressInfo->ai_family, hostAddressInfo->ai_socktype,  hostAddressInfo->ai_protocol);
+
+    if(INVALIDSOCKET(connectionSocket))
+    {
+        int errorCode = GETERROR();
+        #ifdef _WIN32
+       WSACleanup();
+        #endif
+        throw errorCode;
+    }   
+    return true;
+}
+
+bool Networking::Client::Client::ConnectClientSocket()
+{
+int result = connect(connectionSocket, (sockaddr*) &hostAddressInfo->ai_addr, hostAddressInfo->ai_addrlen);
+if (result != 0)
+{
+    int errorCode = GETERROR();
+    CLOSESOCKET(connectionSocket);
+    #ifdef _WIN32
+    WSACleanup();
+    #endif
+    throw errorCode;
+}
+clientIsConnected = true;
+return true;
+
+}
+
+void Networking::Client::Client::SetSocketType(int _pSocketType)
+{
+    addressInfo.ai_socktype = _pSocketType;
+}
+
+void Networking::Client::Client::SetFamily(int _pFamily)
+{
+    addressInfo.ai_family = _pFamily;
+}
+
+void Networking::Client::Client::SetProtocol(int _pProtocol)
+{
+    addressInfo.ai_protocol = _pProtocol;
+}
+
+int Networking::Client::Client::Send(char* _pSendBuffer)
+{
+    int bytesSent = send(connectionSocket,_pSendBuffer, strlen(_pSendBuffer),0 );
+    if(bytesSent == -1)
+    {
+        int errorCode = GETERROR();
+        CLOSESOCKET(connectionSocket);
+        #ifdef _WIN32
+        WSACleanup();
+        #endif
+        throw errorCode;
+    }
+    return bytesSent;
+}
---
+++ src/client/client.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#ifndef _NET_CLIENT_
+#define _NET_CLIENT_
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#pragma comment("Ws2_32.lib")
+#define VERSIONREQUESTED MAKEWORD(2,2)
+#define INVALIDSOCKET(s) ((s) == INVALID_SOCKET)
+#define GETERROR() (WSAGetLastError())
+#define CLOSESOCKET(s) (closesocket(s))
+#else 
+#include  <sys/socket.h>
+#include <netinet/in.h>
+#include  <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
+#include <netdb.h>
+#define SOCKET int
+#define INVALIDSOCKET(s) ((s) < 0)
+#define GETERROR() (errno)
+#define CLOSESOCKET(s) (close(s))
+#define ZeroMemory(dest, count) (memset(dest,0,count))
+typedef const char* PCSTR;
+#endif
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace Networking{
+
+class Client{
+public:
+    Client();
+    Client(char* _pHost, int _pPortNumber);
+    ~Client();
+    bool InitClientSocket();
+    bool CreateClientTCPSocket(char* _pHost, int pPort);
+    bool CreateClientUDPSocket(char* _pHost, int pPort);
+    bool CreateClientSocket(char* _pHost, int pPort);
+    bool ConnectClientSocket();
+    void SetSocketType(int _pSocktype);
+    void SetProtocol(int _pProtocol);
+    void SetFamily(int _pFamily);
+   int Send(char* _pSendBuffer);
+    std::vector<char> Receive();
+    bool Disconnect();
+    bool IsConnected();
+
+private:
+bool clientIsConnected =false;
+SOCKET connectionSocket;
+#ifdef _WIN32
+WSADATA wsaData;
+#endif
+addrinfo addressInfo;
+addrinfo* hostAddressInfo;
+
+    };
+
+}
+
+#endif
